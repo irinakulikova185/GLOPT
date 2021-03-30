42715a52bf0fe8467a1d53c463ff9d9780279838
rinakulikova185 <irinakulikova0185@gmail.com>
2021-03-30 16:57:07 +0300

    change buildcopy

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,108 @@
+//  const gulp = require('gulp');
+//  const browserSync = require('browser-sync').create(); 
+//  const sass = require('gulp-sass');
+//  const rename = require("gulp-rename");
+//  const autoprefixer = require('gulp-autoprefixer');
+//  const cleancss = require('gulp-clean-css');
+//  const imagemin = require('gulp-imagemin');
+//  const newer = require('gulp-newer');
+//  const webpack = require("webpack-stream");
+
+// // const dist = "./dist/";
+// const dist = "C:/OpenServer/domains/Test";
+
+// function copyHtml() {
+//   return gulp.src("./src/index.html")
+//   .pipe(gulp.dest(dist))
+//   .pipe(browserSync.stream());
+// }
+
+//  function browsersync() {
+//  	browserSync.init({ 
+//     server: "./dist/"
+  
+//  	});
+//  }
+
+//  function buildJs() {
+//     return gulp.src("src/js/main.js")
+//                     .pipe(webpack({
+//                         mode: 'development',
+//                         output: {
+//                             filename: 'bundle.js'
+//                         },
+//                         watch: false,
+//                         devtool: "source-map",
+//                         module: {
+//                             rules: [
+//                               {
+//                                 test: /\.m?js$/,
+//                                 exclude: /(node_modules|bower_components)/,
+//                                 use: {
+//                                   loader: 'babel-loader',
+//                                   options: {
+//                                     presets: [['@babel/preset-env', {
+//                                         debug: true,
+//                                         corejs: 3,
+//                                         useBuiltIns: "usage"
+//                                     }]]
+//                                   }
+//                                 }
+//                               }
+//                             ]
+//                           }
+//                     }))
+//                     .pipe(gulp.dest(dist))
+//                     .pipe(browserSync.stream());
+//   }
+
+//  function styles() {
+//  	return gulp.src("src/sass/**/*.+(scss|sass)")
+//  	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+//  	.pipe(rename({suffix: '.min', prefix: ''}))
+//  	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
+//  	.pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Минифицируем стили
+//  	.pipe(gulp.dest(dist)) // Выгрузим результат в папку "app/css/"
+//  	.pipe(browserSync.stream()) // Сделаем инъекцию в браузер
+//  }
+//  function images() {
+//  	return gulp.src('src/images/original/*') // Берём все изображения из папки источника
+//  	.pipe(newer('src/images/newer/')) // Проверяем, было ли изменено (сжато) изображение ранее
+//  	.pipe(imagemin()) // Сжимаем и оптимизируем изображеня
+//  	.pipe(gulp.dest(dist)) // Выгружаем оптимизированные изображения в папку назначения
+//  }
+//  function startwatch() { //следим за изменениями и запускаем соответствующую функцию
+//      gulp.watch(['src/**/*.js', '!src/js/bundle.js'], gulp.parallel(buildJs));
+//      gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel(styles));
+//      gulp.watch('src/*.html', gulp.parallel(copyHtml));
+//      gulp.watch('src/images/origin/*', gulp.parallel(images));
+//  }
+
+//  function buildcopy() {
+// 	return gulp.src([ // Выбираем нужные файлы
+//     'src/css/**/*.min.css',
+//     'src/css/**/*.min.css.map',
+//     'src/js/bundle.js',
+//     'src/js/bundle.js.map',
+// 		'src/images/newer/**/*',
+//         'src/**/*.html',
+//         'src/icons/**/*',
+// 		], { base: 'src' }) // Параметр "base" сохраняет структуру проекта при копировании
+// 	.pipe(gulp.dest(dist)) // Выгружаем в папку с финальной сборкой
+// }
+
+
+
+//  exports.browsersync = browsersync;
+//  exports.styles = styles;
+//  exports.images = images;
+//  exports.startwatch = startwatch;
+//  exports.buildJs = buildJs;
+//  exports.buildcopy = buildcopy;//Выгрузка финальной сборки в dist
+//  exports.copyHtml = copyHtml;  
+// //  exports.build = gulp.parallel(copyHtml, buildJs, styles);
+//  exports.default = gulp.parallel(startwatch, copyHtml, buildJs, styles, browsersync);
+
  const gulp = require('gulp');
  const browserSync = require('browser-sync').create(); 
  const sass = require('gulp-sass');
@@ -8,8 +113,8 @@
  const newer = require('gulp-newer');
  const webpack = require("webpack-stream");
 
-// const dist = "./dist/";
-const dist = "C:/OpenServer/domains/Test";
+const dist = "./dist/";
+// const dist = "C:/OpenServer/domains/Test";
 
  function browsersync() {
  	browserSync.init({ 
@@ -92,8 +197,10 @@ const dist = "C:/OpenServer/domains/Test";
  exports.images = images;
  exports.startwatch = startwatch;
  exports.buildJs = buildJs;
- exports.buildcopy = buildcopy;
- exports.build = gulp.parallel(styles, buildJs, images, buildcopy, startwatch);
+ exports.buildcopy = buildcopy;  //Выгрузка финальной сборки в dist
 
- exports.default = gulp.parallel(startwatch, images, styles, browsersync, buildJs);
+
+ exports.default = gulp.parallel(buildcopy, startwatch, images, styles, browsersync, buildJs);
+
+
 
